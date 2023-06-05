5ea692399ecbaa908a78716a99114e2a51000844
Shirel01 <shirel.israel2001@gmail.com>
2023-06-05 14:25:17 +0300

    parse slide and parsepowerpoint to allow reusability   and neeed to make change in query builder if we want to send only one slide to chatgpt

--- open_ai_query_builder.py
+++ open_ai_query_builder.py
@@ -1,3 +1,19 @@
-def build_prompt(query):
-    prompt = f"Explain this slide of powerpoint {query}"
+
+
+def build_prompt(powerpoint_content_list):
+    all_text = []
+
+    for slide_list in powerpoint_content_list:
+        all_text.extend(slide_list)
+
+    prompt = "Explain these slides of PowerPoint: "
+    prompt += ' '.join(all_text)
+
     return prompt
+
+def build_prompt_for_single_slide(slide_content_list: list[str])->str:
+
+    prompt = "Explain this slide of PowerPoint:\n"
+    prompt += ''.join(slide_content_list)
+    return prompt
+
